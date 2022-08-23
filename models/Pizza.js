731acf2c7604f4cac46c0073351e9821ed7afbb2
Jake Schaefbauer <jschaefmn@gmail.com>
2022-08-23 17:20:14 -0500

    finished 18.1.5

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,1 +1,22 @@
 const { Schema, model } = require('mongoose');
+const Pizza = model('Pizza', PizzaSchema);
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: string
+  },
+  createdBy: {
+    type: string
+  },
+  createdAt: {
+    type: Date, 
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: []
+});
+
+module.exports = Pizza;
