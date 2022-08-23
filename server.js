72660f639ffd8c995608e99826ff67fca6683d3d
Jake Schaefbauer <jschaefmn@gmail.com>
2022-08-23 11:54:16 -0500

    another attempt

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/Module-18',
- {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
- } 
-)
+  useUnifiedTopology: true
+});
+
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
